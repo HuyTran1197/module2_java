dd40f5dada696e852a2eb609259bea86adae5ff5
HuyTran1197 <huytran5e@gmail.com>
2025-07-31 22:07:59 +0700

    first commit

--- src/SystemTime.java
+++ src/SystemTime.java
@@ -1,6 +1,8 @@
-public class System {
+import java.util.Date;
+
+public class SystemTime {
     public static void main(String[] args) {
         Date now = new Date();
-        System.out.println("Now is: " +now);
+        System.out.println("Now is: "+now);
     }
 }
