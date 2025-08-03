ea2050c725d76d7c6f572773ac0487408244c8b3
HuyTran1197 <huytran5e@gmail.com>
2025-08-03 12:46:46 +0700

    first commit

--- src/ss1_bai_tap/Exchange.java
+++ src/ss1_bai_tap/Exchange.java
@@ -1,5 +1,5 @@
 package ss1_bai_tap;
-
+import java.util.Scanner;
 public class Exchange {
     double vnd;
     double usd;
@@ -12,6 +12,16 @@ public class Exchange {
         return this.usd * this.vnd;
     }
     public void display(){
-        System.out.println("quy đổi " +this.usd+ "usd sang vnd = "+getUsd());
+        System.out.println("quy đổi " +this.usd+ " usd sang vnd = "+getUsd());
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập tỉ giá vnd");
+        double vnd = Double.parseDouble(scanner.nextLine());
+        System.out.println("Nhập số tiền usd cần đổi:");
+        double usd = Double.parseDouble(scanner.nextLine());
+        Exchange quyDoi = new Exchange(vnd,usd);
+        quyDoi.display();
     }
 }
--- src/ss1_bai_tap/Introduce.java
+++ src/ss1_bai_tap/Introduce.java
@@ -1,5 +1,5 @@
 package ss1_bai_tap;
-
+import java.util.Scanner;
 public class Introduce {
     String name;
     public Introduce(String name){
@@ -7,5 +7,13 @@ public class Introduce {
     }
     public void display(){
         System.out.println("Hello " +this.name);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        Introduce print = new Introduce(name);
+        print.display();
     }
 }
