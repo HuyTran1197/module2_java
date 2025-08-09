01b4b9fab4abfdf179119a58a4d4e9e66ac62f06
HuyTran1197 <huytran5e@gmail.com>
2025-08-09 20:48:11 +0700

    first commit

--- src/ss5_accessmodifier_static/bai_tap/Circle.java
+++ src/ss5_accessmodifier_static/bai_tap/Circle.java
@@ -1,5 +1,7 @@
 package ss5_accessmodifier_static.bai_tap;
 
+import java.util.Scanner;
+
 public class Circle {
     private double radius = 1.0;
     private String color = "red";
@@ -15,6 +17,16 @@ public class Circle {
     }
     protected double getArea(){
         return (getRadius()*getRadius()) * Math.PI;
+    }
+    static class TestCircle{
+        public static void main(String[] args) {
+            Circle circle1 = new Circle(3);
+            System.out.println(circle1.getArea());
+            Scanner scanner = new Scanner(System.in);
+            double radius = Double.parseDouble(scanner.nextLine());
+            Circle circle2 = new Circle(radius);
+            System.out.println(circle2.getArea());
+        }
     }
 
 }
